88f304d7510b7be33ce7ef7585dbf9cffe9c6bb6
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-11-05 07:26:59 -0500

    Update 1-NMT-With-Attention.py

--- 4-Natural-Language-Processing-With-Attention-Models/1-NMT-With-Attention.py
+++ 4-Natural-Language-Processing-With-Attention-Models/1-NMT-With-Attention.py
@@ -49,7 +49,9 @@ VOCAB_SIZE = 12000
 UNITS = 256
 
 class Encoder(tf.keras.layers.Layer):
+    
     def __init__(self,vocab_size,units):
+        
         """
           Initializes an instance of this class
 
@@ -57,5 +59,68 @@ class Encoder(tf.keras.layers.Layer):
             vocab_size (int): Size of the vocabulary
             units (unit): Number of units in the LSTM layer
         """
+        
         super(Encoder,self).__init__()
+        
+        self.embedding = tf.keras.layers.Embedding(input_dim = None,output_dim = None,mask_zero = None)
+        
+        self.rnn = tf.keras.layers.Bidirectional(merge_mode = "sum", layer = tf.keras.layers.None(units = None, return_sequences = None))
+
+
+        def call(self,context):
+            
+            """Forward pass of this layer
+
+            Args: 
+                context (tf.Tensor): The sentence to translate
+            Returns:
+                tf.Tensor: Encoded sentence to translate
+            """
+            
+            x = None
+            x = None
+            return x 
+
+encoder = Encoder(VOCAB_SIZE,UNITS)
+
+
+# pass a batch of sentences to translate from english to portugese
+encoder_output = encoder(to_translate)
+
+
+print(f'Tensor of sentences in english has shape: {to_translate.shape}\n')
+print(f'Encoder output has shape: {enocder_output.shape}')
+
+
+class CrossAttention(tf.keras.layers.Layer):
+    
+    def __init__(self,units):
+        super().__init__()
+        
+        self.mha = (tf.keras.layers.None(key_dim = None,
+                                         num_heads = None))
+        self.layernorm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
+
+    def call(self,context,target):
+        """Forward pass of this layer
+
+        Args:
+            context (tf.Tensor): Encoded sentence to translate
+            target (tf.Tensor): The embedded shifted-to-the-right translation
+
+        Returns:
+            tf.Tensor: Cross attention between context and target
+        """
+
+            attn_output = None(query = None, 
+                               value = None)
+
+            x = self.add([target,attn_output])
+            x = self.layernorm(x)
+            return x 
+        
+        
+
+        
         
